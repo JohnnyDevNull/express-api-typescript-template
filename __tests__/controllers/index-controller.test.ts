e1b36609b3240907513c25fe2abe520d85b58010
JohnnyDevNull <philipp.john@live.de>
2018-08-25 17:35:25 +0200

    improve index-controller test

--- __tests__/controllers/index-controller.test.ts
+++ __tests__/controllers/index-controller.test.ts
@@ -3,31 +3,36 @@ import app from '../../src/app';
 
 describe('index route', () => {
 
-  beforeEach(() => {
-    console.log('beforeEach triggered');
-  });
+  // beforeEach(() => {
+  //   console.log('beforeEach triggered');
+  // });
 
-  afterEach(() => {
-    console.log('afterEach triggered');
-  });
+  // afterEach(() => {
+  //   console.log('afterEach triggered');
+  // });
 
-  beforeAll(() => {
-    console.log('beforeAll triggered');
-  });
+  // beforeAll(() => {
+  //   console.log('beforeAll triggered');
+  // });
 
-  afterAll(() => {
-    console.log('afterAll triggered');
+  // afterAll(() => {
+  //   console.log('afterAll triggered');
+  // });
+
+  test('app to be defined', () => {
+    expect(app).toBeDefined();
   });
 
   test('test response statusCode', () => {
+    expect.assertions(1);
     return request(app).get('/').then(response => {
       expect((<any>response).statusCode).toBe(200);
     });
   });
 
-  test('test response body', () => {
-    return request(app).get('/').then(response => {
-      expect((<any>response).body).toEqual({meta: {code: 0, message: 'The api is online!'}});
-    });
+  test('test response body', async () => {
+    expect.assertions(1);
+    const response = await request(app).get('/');
+    expect((<any>response).body).toEqual({meta: {code: 0, message: 'The api is online!'}});
   });
 });
